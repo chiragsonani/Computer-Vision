fa24331ed9bbf60fe7f9ee103c14ff9f3d7e5984
chiragsonani <chiragsonani25@gmail.com>
2025-04-26 10:35:29 +0200

    Initial commit

---
+++ Motion Capturing.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+video = cv.VideoCapture(0) # for webcam
+# video = cv.VideoCapture('/home/chirag/Desktop/Computer Vision/lane_video.mp4') # for video file
+substractor = cv.createBackgroundSubtractorMOG2(40,30)
+
+while True:
+    ret, frame = video.read()
+    if ret:
+        mask = substractor.apply(frame)
+        cv.imshow('Mask', mask)
+
+        if cv.waitKey(5)== ord('1'):
+            break
+    else:
+        # video = cv.VideoCapture('/home/chirag/Desktop/Computer Vision/lane_video.mp4')
+        video = cv.VideoCapture(0)
+        
+cv.destroyAllWindows()
+video.release()
