29612a3b0bc03ea43e9e55c49b130ffc679b6997
chiragsonani <chiragsonani25@gmail.com>
2025-04-26 15:43:27 +0200

    Initial commit

--- Image Thresholding.py
+++ Image Thresholding.py
@@ -1,4 +1,6 @@
-import cv2 as cv
+#To make images clearer and easier to read
+
+# import cv2 as cv
 # import numpy as np
 
 img = cv.imread('/home/chirag/Desktop/Computer Vision/source/book_page.png')
